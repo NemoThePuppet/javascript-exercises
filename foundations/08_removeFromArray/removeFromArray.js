880f2033303adaab082b30ac3d3db5b89b48c906
NemoThePuppet <43538298+NemoThePuppet@users.noreply.github.com>
2026-03-19 14:29:30 +0100

    Complete exercise 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    let filteredArr = theArgs[0];
+    for(let i = 1; i <= theArgs.length;i++){
+        filteredArr = filteredArr.filter((item) => item!==theArgs[i])
+    }
+    console.log(filteredArr)
+    return filteredArr
 };
 
 // Do not edit below this line
