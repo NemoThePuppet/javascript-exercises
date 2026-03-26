dda21cb163c2425d58c6cf842e49b3cf48279299
NemoThePuppet <43538298+NemoThePuppet@users.noreply.github.com>
2026-03-26 13:58:49 +0100

    Complete exercise 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num)=>sum+num,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, num)=>mul*num,1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for (let i = a; i > 1; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
