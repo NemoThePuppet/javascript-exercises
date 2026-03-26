05950abedc39fcac773d7067ff4736367c168f0e
NemoThePuppet <43538298+NemoThePuppet@users.noreply.github.com>
2026-03-26 15:18:01 +0100

    Complete exercise 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 0) return 0;
+    if (n < 0) return "OOPS";
+    // let arr = [1, 1];
+    // for(let i = 2; i<=n-1; i++){
+    //   arr.push(arr[i-1] + arr[i-2]);
+    //   console.log(arr)
+    // }
+    // return arr[n-1]
 
+    if(n>2){
+        return fibonacci(n-1)+fibonacci(n-2)
+    }
+    return 1;
 };
 
 // Do not edit below this line
