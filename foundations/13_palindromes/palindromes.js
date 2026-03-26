b9eb5cdce65e49af48ba9e43b7dad0620550490a
NemoThePuppet <43538298+NemoThePuppet@users.noreply.github.com>
2026-03-26 14:42:26 +0100

    Complete exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().trim().replace(/[\.,?!\s]/g, "");
+    let reversedWord = "";
+    for(let i=word.length-1; i>=0;i--){
+        reversedWord += word[i];
+    }
+    if (word === reversedWord) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
