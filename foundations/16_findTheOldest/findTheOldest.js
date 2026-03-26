71f08f0073d48d428a53879230bbcf8f1b34de7e
NemoThePuppet <43538298+NemoThePuppet@users.noreply.github.com>
2026-03-26 16:06:25 +0100

    Complete exercise 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,37 @@
-const findTheOldest = function() {
+// const people = [
+//       {
+//         name: "Carly",
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//       },
+//       {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//       },
+//       {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//       },
+//     ]
 
+function getAge(birth, death){
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person)=>{
+        let personAge = getAge(person.yearOfBirth,person.yearOfDeath);
+        let oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+        if(!("name" in oldest) || personAge > oldestAge){
+            return person;
+        }
+        return oldest;
+    },{})
 };
 
 // Do not edit below this line
