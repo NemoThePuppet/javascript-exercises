14534becd1a5944e3371df8d295f298bcf3d8304
NemoThePuppet <43538298+NemoThePuppet@users.noreply.github.com>
2026-03-26 15:29:17 +0100

    Complete exercise 15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // return books.reduce((titles,book)=>{
+    //     titles.push(book.title);
+    //     return titles;
+    // },[])
+    return books.map((book)=>book.title)
 };
 
 // Do not edit below this line
